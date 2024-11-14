d259c04605689262abaed3005c5ca196658773ba
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-11-14 14:34:58 +0300

    fix clerk

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { middleware } from "@clerk/nextjs";
 
-export default clerkMiddleware();
+export default middleware({
+  // Optional configuration
+  publicRoutes: ["/", "/api/public"],
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
