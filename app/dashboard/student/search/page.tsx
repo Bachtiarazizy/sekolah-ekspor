9b79cb5f6020915ffbdaaf30aa6e0973dea7456e
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-11-15 09:46:57 +0300

    searchs

--- app/dashboard/student/search/page.tsx
+++ app/dashboard/student/search/page.tsx
@@ -1,47 +1,53 @@
-import { getCourses } from "@/actions/get-courses";
-import CoursesList from "@/components/course/course-list";
-import prisma from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
+"use client";
 
-import { redirect } from "next/navigation";
-import Categories from "./_components/categories";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import SearchInput from "@/components/search-input";
+import CoursesList from "@/components/course/course-list";
+import Categories from "./_components/categories";
+import { getCourses } from "@/actions/get-courses";
 
-interface SearchPageProps {
-  searchParams: {
+interface ClientSearchPageProps {
+  initialCategories: Category[];
+  initialSearchParams: {
     title: string;
     categoryId: string;
   };
 }
 
-const Searchpage = async ({ searchParams }: SearchPageProps) => {
-  const { userId } = await auth();
+const ClientSearchPage = ({ initialCategories, initialSearchParams }: ClientSearchPageProps) => {
+  const searchParams = useSearchParams();
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (!userId) {
-    return redirect("/");
-  }
-  const categories = await prisma.category.findMany({
-    orderBy: {
-      name: "asc",
-    },
-  });
+  useEffect(() => {
+    const fetchCourses = async () => {
+      setIsLoading(true);
 
-  const courses = await getCourses({
-    userId: userId,
-    ...searchParams,
-  });
+      const updatedCourses = await getCourses({
+        userId: auth().userId,
+        title: searchParams.get("title") || initialSearchParams.title,
+        categoryId: searchParams.get("categoryId") || initialSearchParams.categoryId,
+      });
+
+      setCourses(updatedCourses);
+      setIsLoading(false);
+    };
+
+    fetchCourses();
+  }, [searchParams]);
 
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <Categories items={categories} />
+      <Categories items={initialCategories} />
       <div className="px-6 overflow-y-scroll bg-secondary mx-0 md:mx-auto py-6 rounded-none md:h-[calc(100vh-170px)] md:rounded-none md:rounded-bl-3xl md:mt-1 md:rounded-tl-xl container">
-        <CoursesList items={courses} />{" "}
+        {isLoading ? <p>Loading...</p> : <CoursesList items={courses} />}
       </div>
     </>
   );
 };
 
-export default Searchpage;
+export default ClientSearchPage;
